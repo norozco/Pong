6e46fb6f848eb191de8a3c61f806a6bf4c3b763d
norozco <41894620+norozco@users.noreply.github.com>
2022-07-31 19:49:33 -0500

    Update pong.js

--- pong.js
+++ pong.js
@@ -9,14 +9,24 @@ const Pong ={
             height : 1000
         },
         stageStyle : {
-            border : "4px solid white"
+            border : "4px solid white",
+            position : "relative"
         },
         gameStyle : {
             background : "black",
             padding : "100px",
             height : "100vh"
     
-        }
+        },
+        ballStyle : {
+            position : "absolute"
+        }, 
+        intervalSpeed : 13, //Leave this alone unless there is lag
+        ballSpeed : 3 // This is obviously ball speed :D 
+    },
+    ballXy : {
+        x : 100,
+        y : 100
     },
 
     setupGame : function(){
@@ -35,13 +45,38 @@ const Pong ={
         z.ball.height(z.config.ballDimensions.height)
         //actually added to the page
         z.stage.append(z.ball);
-    
+        z.ball.css(z.config.ballStyle)
         z.gamediv.css(z.config.gameStyle)
         z.stage.css(z.config.stageStyle)
+        z.nextBallMove()
+
+        function repeatBallMove(){
+            //Move the ball one increment
+            z.nextBallMove()
+            //wait some time and then start all over again
+            setTimeout(function(){
+                repeatBallMove()
+            },z.config.intervalSpeed)
+        }
+        //Start ball movement
+        repeatBallMove()   
+    },
+
+    moveBall : function(){
+        const z = this
+        z.ball.css({
+            left : z.ballXy.x+"px",
+            top : z.ballXy.y +"px" 
+        })
+
+    },
+
+    nextBallMove : function(){
+        const z = this
+        z.ballXy.x = z.ballXy.x + z.config.ballSpeed;
+        z.ballXy.y = z.ballXy.y + z.config.ballSpeed;
+        z.moveBall()
     }
-
-
-
 
 }
 
@@ -64,4 +99,6 @@ console.log(addnumbers (6,25))
 
 
 
-$(Pong.setupGame)
+$(function(){
+    Pong.setupGame ()
+})
