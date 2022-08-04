5d98096aaabe82c4e8098e914fb8fd1fd5edbb34
fluffybunnies <alec@cloudkitchens.com>
2022-08-04 16:45:03 -0700

    Lesson 2 Prep

--- pong.js
+++ pong.js
@@ -5,8 +5,8 @@ const Pong ={
             height : 20 
         },
         stageDimensions : {
-            width : 1500, 
-            height : 1000
+            width : 1000, 
+            height : 600
         },
         stageStyle : {
             border : "4px solid white",
@@ -16,19 +16,23 @@ const Pong ={
             background : "black",
             padding : "100px",
             height : "100vh"
-    
         },
         ballStyle : {
             position : "absolute"
         }, 
-        intervalSpeed : 13, // Leave this alone unless there is lag
-        ballSpeed : 3 // This is obviously ball speed :D 
+        intervalSpeed : 13 // Leave this alone unless there is lag
     },
     ballPos : {
         x : 100,
         y : 100
     },
-
+    ballSpeed: {
+    	x: 4,
+    	y: 2
+    },
+    garbageCollection: {
+    	animationInterval: null
+    },
 
     setupGame : function(containerElement){
         const z = this
@@ -58,26 +62,48 @@ const Pong ={
 
 
         // Start ball movement
-        z.nextBallMove() 
+        z.startBallMove()
+    },
+
+    startBallMove: function(){
+    	const z = this
+    	z.stopBallMove()
+    	z.garbageCollection.animationInterval = setInterval(function(){
+    		z.nextBallMove()
+    	}, z.config.intervalSpeed)
+    },
+
+    stopBallMove: function(){
+    	clearInterval(this.garbageCollection.animationInterval)
     },
 
     nextBallMove: function(){
         const z = this
-
-        // Move the ball one increment
         z.calculateNextBallPosition()
         z.refreshBallPositionOnScreen()
-
-        // wait some time and then start all over again
-        setTimeout(function(){
-            z.nextBallMove()
-        }, z.config.intervalSpeed)
     },
 
     calculateNextBallPosition : function(){
         const z = this
-        z.ballPos.x = z.ballPos.x + z.config.ballSpeed;
-        z.ballPos.y = z.ballPos.y + z.config.ballSpeed;
+        z.ballPos.x += z.ballSpeed.x;
+        z.ballPos.y += z.ballSpeed.y;
+
+        // Check if ball is about to hit the left or right wall
+        const leftWall = 0
+        const rightWall = z.config.stageDimensions.width - z.config.ballDimensions.width
+        if (z.ballPos.x < leftWall || z.ballPos.x > rightWall) {
+        	// Simply reverse the X velocity
+        	z.ballSpeed.x *= -1
+        }
+
+				// Check if ball is about to hit the top or bottom wall
+        const topWall = 0
+        const bottomWall = z.config.stageDimensions.height - z.config.ballDimensions.height
+        if (z.ballPos.y < topWall || z.ballPos.y > bottomWall) {
+        	// Simply reverse the Y velocity
+        	z.ballSpeed.y *= -1
+        }
+
     },
 
     refreshBallPositionOnScreen : function(){
@@ -89,25 +115,6 @@ const Pong ={
     }
 
 }
-
-
-
-// Training stuff - can delete later
-function addnumbers(number1,number2){
-    const result=number1 * number2;
-    return result
-
-
-}
-
-console.log('Hi its me')
-console.log(addnumbers (6,25))
-
-
-
-
-
-
 
 
 
