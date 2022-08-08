459cbebe941c3e452410fcb0a0461b6b00d7ffa2
norozco <41894620+norozco@users.noreply.github.com>
2022-08-07 23:47:34 -0500

    Update pong.js

--- pong.js
+++ pong.js
@@ -1,81 +1,82 @@
-const Pong ={
-    config :{
-        ballDimensions : {
-            width : 200, 
-            height : 100 
+const Pong = {
+    config: {
+        ballDimensions: {
+            width: 200,
+            height: 100
         },
-        stageDimensions : {
-            width : 1500, 
-            height : 1000
+        stageDimensions: {
+            width: 1500,
+            height: 1000
         },
-        stageStyle : {
-            border : "4px solid white",
-            position : "relative"
+        stageStyle: {
+            border: "4px solid white",
+            position: "relative"
         },
-        gameStyle : {
-            background : "black",
-            padding : "100px",
-            height : "100vh"
-    
-        },
-        ballStyle : {
-            position : "absolute"
-        }, 
-        intervalSpeed : 13, // Leave this alone unless there is lag
+        gameStyle: {
+            background: "black",
+            padding: "100px",
+            height: "100vh"
 
-        paddleStyle :{
-            background : "white",
+        },
+        ballStyle: {
+            position: "absolute"
+        },
+        intervalSpeed: 13, // Leave this alone unless there is lag
+
+        paddleStyle: {
+            background: "white",
             width: "7px",
             height: "200px",
             position: "absolute"
         },
 
-        paddleOnePosition :{
+        paddleOnePosition: {
             left: "10px"
         },
 
-        paddleTwoPosition :{
+        paddleTwoPosition: {
             right: "10px"
 
         },
 
         paddingOneInitialSpeed: {
-        	y: 3
+            y: 3
         },
         paddingTwoInitialSpeed: {
-        	y: 3
+            y: 3
         },
 
     },
-    ballPos : {
-        x : 100,
-        y : 100
+
+    ballPos: {
+        x: 100,
+        y: 100
     },
 
-    ballSpeed :{
+    ballSpeed: {
         x: 3,
         y: 1
     },
-    paddleOnePosition :{
+    paddleOnePosition: {
         y: 0
 
     },
-    
-    paddleTwoPosition :{
+
+    paddleTwoPosition: {
         y: 0
     },
 
-    paddleOneSpeed :{
+    paddleOneSpeed: {
         y: 5
     },
-    
-    paddleTwoSpeed :{
-        y: 0
+
+    paddleTwoSpeed: {
+        y: 5
     },
 
-    ballInterval : null,
+    ballInterval: null,
 
-    setupGame : function(containerElement){
+    setupGame: function (containerElement) {
         const z = this
         z.gamediv = containerElement;
 
@@ -88,13 +89,13 @@ const Pong ={
         z.paddleControls()
         // Start ball movement
         z.startBallMove()
-        
+
     },
 
-    createStage : function(){
+    createStage: function () {
         const z = this
         // create brand new div for stage
-        z.stage = $ ("<div></div>") 
+        z.stage = $("<div></div>")
         z.stage.width(z.config.stageDimensions.width)
         z.stage.height(z.config.stageDimensions.height)
         // set stage styles
@@ -104,10 +105,10 @@ const Pong ={
 
     },
 
-    createBall : function(){
-        const z= this
+    createBall: function () {
+        const z = this
         // create brand new ball
-        z.ball = $ ("<img src=\"./dvd.png\" />")
+        z.ball = $("<img src=\"./dvd.png\" />")
         z.ball.width(z.config.ballDimensions.width)
         z.ball.height(z.config.ballDimensions.height)
         // set ball styles
@@ -117,42 +118,42 @@ const Pong ={
 
     },
 
-    createPaddleOne : function(){
+    createPaddleOne: function () {
         const z = this
         // create paddle
-        z.paddleOne = $ ("<div></div>")
+        z.paddleOne = $("<div></div>")
         // assigning styles to the file
         z.paddleOne.css(z.config.paddleStyle)
         // add paddle to stage
         z.paddleOne.css(z.config.paddleOnePosition)
         z.stage.append(z.paddleOne);
-        console.log ('help')
+        console.log('help')
     },
 
-    createPaddleTwo : function(){
+    createPaddleTwo: function () {
         const z = this
         // create paddle
-        z.paddleTwo = $ ("<div></div>")
+        z.paddleTwo = $("<div></div>")
         // assigning styles to the file
         z.paddleTwo.css(z.config.paddleStyle)
         // add paddle to stage
         z.paddleTwo.css(z.config.paddleTwoPosition)
         z.stage.append(z.paddleTwo);
 
-        console.log ('help')
+        console.log('help')
     },
 
-    startBallMove: function(){
-         // This calls next ball move repeadeatly, in this case its better to use than setTimeout
+    startBallMove: function () {
+        // This calls next ball move repeadeatly, in this case its better to use than setTimeout
         const z = this
-        z.ballInterval = setInterval(function(){
+        z.ballInterval = setInterval(function () {
             z.nextBallMove()
-            
+
         }, z.config.intervalSpeed)
 
     },
 
-    stopBallMove: function(){
+    stopBallMove: function () {
         const z = this
 
         clearInterval(z.ballInterval)
@@ -160,7 +161,7 @@ const Pong ={
 
     },
 
-    nextBallMove: function(){
+    nextBallMove: function () {
         const z = this
 
         // Move the ball one increment
@@ -169,105 +170,142 @@ const Pong ={
 
     },
 
-    calculateNextBallPosition : function(){
-        const z = this 
+    calculateNextBallPosition: function () {
+        const z = this
         z.ballPos.x += z.ballSpeed.x // += add to myself
         z.ballPos.y += z.ballSpeed.y
         //check if ball is about to hit the right wall
-        if (z.ballPos.x > z.config.stageDimensions.width - z.config.ballDimensions.width){
+        if (z.ballPos.x > z.config.stageDimensions.width - z.config.ballDimensions.width) {
             //reverse x direction
             z.ballSpeed.x *= -1 // *= Multyplying myself
-        
+
         }
-         //check if ball is about to hit the left wall  
-        if (z.ballPos.x < 0){
+        //check if ball is about to hit the left wall  
+        if (z.ballPos.x < 0) {
             //reverse x direction
             z.ballSpeed.x *= -1 // *= Multyplying myself
-        
+
 
         }
         // check if ball is about to hit the bottom 
-        if (z.ballPos.y > z.config.stageDimensions.height - z.config.ballDimensions.height){
+        if (z.ballPos.y > z.config.stageDimensions.height - z.config.ballDimensions.height) {
             //reverse y direction
             z.ballSpeed.y *= -1 // *= Multyplying myself
-         
+
 
         }
-        if (z.ballPos.y < 0){
-           //reverse y direction
+        if (z.ballPos.y < 0) {
+            //reverse y direction
             z.ballSpeed.y *= -1 // *= Multyplying myself
-           
+
 
         }
     },
 
-    refreshBallPositionOnScreen : function(){
+    refreshBallPositionOnScreen: function () {
         const z = this
         z.ball.css({
-            left : z.ballPos.x + "px",
-            top : z.ballPos.y + "px" 
+            left: z.ballPos.x + "px",
+            top: z.ballPos.y + "px"
         })
     },
 
-    calculateNextPaddleOnePosition : function(direction){
+    calculateNextPaddleOnePosition: function (direction) {
         const z = this
-            if(direction == "down"){
-                z.paddleOnePosition.y += z.paddleOneSpeed.y
-            } else{
-                z.paddleOnePosition.y += -z.paddleOneSpeed.y
-            }
-            
+        if (direction == "down") {
+            z.paddleOnePosition.y += z.paddleOneSpeed.y
+        } else {
+            z.paddleOnePosition.y += -z.paddleOneSpeed.y
+        }
+
         console.log(direction)
     },
 
-    refreshPaddlePositionOnScreen : function(){
+    refreshPaddleOnePositionOnScreen: function () {
         const z = this
         z.paddleOne.css({
-            top : z.paddleOnePosition.y + "px" 
+            top: z.paddleOnePosition.y + "px"
         })
     },
-    
+
+    calculateNextPaddleTwoPosition: function (direction) {
+        const z = this
+        if (direction == "down") {
+            z.paddleTwoPosition.y += z.paddleTwoSpeed.y
+        } else {
+            z.paddleTwoPosition.y += -z.paddleTwoSpeed.y
+        }
+
+        console.log(direction)
+    },
+
+    refreshPaddleTwoPositionOnScreen: function () {
+        const z = this
+        z.paddleTwo.css({
+            top: z.paddleTwoPosition.y + "px"
+        })
+    },
     /**
-    	TO DO:
-    		Set up interval that actually moves the paddle
-    		(Cuz the code below is only setting where it SHOULD be moving,
-    		not where it actually is)
+        TO DO:
+            Set up interval that actually moves the paddle
+            (Cuz the code below is only setting where it SHOULD be moving,
+            not where it actually is)
     */
-    paddleControls :function(){
+    paddleControls: function () {
         console.log("paddle Controls Not Working")
         const z = this
         // Setup paddle one controls
 
         // If key up or down is pressed
         // Note: keypress might make more sense, research later
-        $ (window).keydown(function(event){
+        $(window).keydown(function (event) {
             //alert(event.which)
             //s=83 w=87 (on keyboard)
-
+            console.log(event.which)
             //if you press up 
-             if (event.which == 87){
-             		// Set velocity to UP
-            		//z.paddleOneSpeed.y = -1 * z.config.paddingOneInitialSpeed
+            if (event.which == 87) {
+                // Set velocity to UP
+                //z.paddleOneSpeed.y = -1 * z.config.paddingOneInitialSpeed
                 z.calculateNextPaddleOnePosition("up")
-                z.refreshPaddlePositionOnScreen()
-                console.log( )
-             }
-        //If you press down
-            if (event.which == 83){
-            	  // Set velocity to DOWN
+                z.refreshPaddleOnePositionOnScreen()
+
+            }
+            //If you press down
+            if (event.which == 83) {
+                // Set velocity to DOWN
                 //z.paddleOneSpeed.y = z.config.paddingOneInitialSpeed
                 z.calculateNextPaddleOnePosition("down")
-                z.refreshPaddlePositionOnScreen()
-            }  
+                z.refreshPaddleOnePositionOnScreen()
+            }
+
+            if (event.which == 38) {
+                // Set velocity to UP
+                //z.paddleOneSpeed.y = -1 * z.config.paddingOneInitialSpeed
+                event.preventDefault()
+                z.calculateNextPaddleTwoPosition("up")
+                z.refreshPaddleTwoPositionOnScreen()
+
+                console.log("pressing up")
+            }
+            //If you press down
+            if (event.which == 40) {
+                // Set velocity to DOWN
+                //z.paddleOneSpeed.y = z.config.paddingOneInitialSpeed
+                event.preventDefault()
+                z.calculateNextPaddleTwoPosition("down")
+                z.refreshPaddleTwoPositionOnScreen()
+                console.log("pressing down")
+            }
+
 
         })
 
         // If either up or down key is let go
-        $ (window).keyup(function(event){
-        	// Set Y movement to 0
-             if (event.which == 87 || event.which == 83){
-            		//z.paddleOneSpeed.y = 0
-             }
+        $(window).keyup(function (event) {
+            // Set Y movement to 0
+            if (event.which == 87 || event.which == 83) {
+                //z.paddleOneSpeed.y = 0
+            }
         })
 
 
@@ -279,16 +317,16 @@ const Pong ={
 
 
 // Training stuff - can delete later
-function addnumbers(number1,number2){
-    const result=number1 * number2;
+function addnumbers(number1, number2) {
+    const result = number1 * number2;
     return result
 
 
 }
 
 console.log('Hi its me')
-console.log(addnumbers (6,25))
-let speed= 5 
+console.log(addnumbers(6, 25))
+let speed = 5
 console.log('hi speed=', speed)
 speed *= -2
 console.log('new speed', speed)
